d141637ff82736c085ff60f2c08b6b5ca7e3a323
Richard Rice <ricerichardj@gmail.com>
2021-07-07 21:07:13 -0400

    Spring Initializr files

---
+++ src/test/java/rjr/secretgiftexchange/SecretGiftExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package rjr.secretgiftexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretGiftExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
