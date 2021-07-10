b73611c624b7cbd17b2a149c778b4088864ae596
Richard Rice <ricerichardj@gmail.com>
2021-07-10 13:13:30 -0400

    add default exception handlers

--- src/main/java/rjr/secretgiftexchange/exception/RestExceptionHandler.java
+++ src/main/java/rjr/secretgiftexchange/exception/RestExceptionHandler.java
@@ -3,9 +3,10 @@ package rjr.secretgiftexchange.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	protected ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException e) {
