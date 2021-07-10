291c4c1fa44d1587ba97c703ff480bf61cf5c3a0
Richard Rice <ricerichardj@gmail.com>
2021-07-10 00:19:29 -0400

    set up integration testing, add readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # secret-gift-exchange
+
+To run:
+```sh
+./gradlew clean build bootRun
+```
+
+In a second terminal, in order to quick reload after code changes:
+```sh
+./gradlew build -x test
+```
+
+Sample curl commands:
+```sh
+curl localhost:8080/members
+curl localhost:8080/members/0
+curl -X POST localhost:8080/members -d '{"name":"Bob the Builder"}' -H 'Content-Type:application/json'
+curl -X POST localhost:8080/members -d '{"name":"Great Sphinx"}' -H 'Content-Type:application/json'
+curl localhost:8080/members
+curl localhost:8080/members/0
+curl localhost:8080/members/1
+curl -X PUT localhost:8080/members/1 -d '{"name":"Big Bro"}'
+curl localhost:8080/members
+curl -X DELETE localhost:8080/members/0
+```
+
+Run integration tests:
+```sh
+./gradlew clean build test -i
+```
--- src/test/java/rjr/secretgiftexchange/SecretGiftExchangeApplicationTests.java
+++ src/test/java/rjr/secretgiftexchange/SecretGiftExchangeApplicationTests.java
@@ -1,13 +1,57 @@
 package rjr.secretgiftexchange;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import rjr.secretgiftexchange.model.Member;
+import rjr.secretgiftexchange.model.MemberRequest;
+import rjr.secretgiftexchange.repository.MemberRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SecretGiftExchangeApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+  
+	@Autowired
+	private MemberRepository memberRepository;
+
 	@Test
-	void contextLoads() {
+	void testAddMember() throws Exception {
+		mockMvc.perform(get("/members"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("[]"));
+
+		MemberRequest firstMember = new MemberRequest("Bob the Builder");
+		mockMvc.perform(post("/members")
+				.contentType("application/json")
+				.content(objectMapper
+				.writeValueAsString(firstMember))
+			).andExpect(status().isOk());
+
+		mockMvc.perform(get("/members"))
+				.andExpect(status().isOk())
+				.andExpect(content().json("[{\"id\":0,\"name\":\"Bob the Builder\"}]"));
+
+		Member m = memberRepository.getMemberById(0);
+		assertEquals(m.getId(), 0);
+		assertEquals(m.getName(), "Bob the Builder");
 	}
 
 }
